5fba1e084c05736be1b4f5be7b25aecb2bc72856
Simon Ganon <simon@blueboard.io>
2018-12-21 10:41:29 +0100

    test: unit testing of send methods on problems

--- common/payload_test.go
+++ common/payload_test.go
@@ -2,6 +2,7 @@ package common_test
 
 import (
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/sganon/go-request/common"
@@ -36,4 +37,24 @@ func TestInputProblemError(t *testing.T) {
 func TestInputUnexpectedError(t *testing.T) {
 	prob := common.UnexpectedProblem{}
 	assert.Equal(t, common.ErrUnexpected.Error(), prob.Error())
+}
+
+func TestInputProblemSend(t *testing.T) {
+	w := httptest.NewRecorder()
+	prob := common.InputProblem{
+		Payload: common.Payload{
+			Title:  "Test problem",
+			Status: 400,
+		},
+	}
+	prob.Send(w)
+}
+
+func TestUnexpectedProblemSend(t *testing.T) {
+	w := httptest.NewRecorder()
+	prob := common.UnexpectedProblem{
+		Title:  "Test problem",
+		Status: 500,
+	}
+	prob.Send(w)
 }
