8cb2675197d8decf25f2f3b1de8edd966e124a01
Simon Ganon <simon@blueboard.io>
2019-08-21 19:16:39 +0200

    feat: Decode une interface{}, created DecodeAndValidate

--- request.go
+++ request.go
@@ -4,15 +4,15 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/sganon/go-request/problem"
 	"github.com/sganon/go-request/form"
+	"github.com/sganon/go-request/problem"
 )
 
 type Output interface {
 	Validate() []problem.ParamError
 }
 
-func Decode(r *http.Request, formOutput Output, bodyOutput Output) problem.Problem {
+func Decode(r *http.Request, formOutput interface{}, bodyOutput interface{}) problem.Problem {
 	var ok bool
 	var inputProblem *problem.Input
 	var unexpectedProblem *problem.UnexpectedProblem
@@ -25,6 +25,30 @@ func Decode(r *http.Request, formOutput Output, bodyOutput Output) problem.Probl
 		if unexpectedProblem, ok = err.(*problem.UnexpectedProblem); ok && unexpectedProblem != nil {
 			return unexpectedProblem
 		}
+	}
+	if bodyOutput != nil {
+		bodyDecoder := json.NewDecoder(r.Body)
+		defer r.Body.Close()
+		err := bodyDecoder.Decode(&bodyOutput)
+		if err != nil {
+			return &problem.DefaultUnexpected
+		}
+	}
+	if inputProblem == nil {
+		return nil
+	}
+	return inputProblem
+}
+
+func DecodeAndValidate(r *http.Request, formOutput Output, bodyOutput Output) problem.Problem {
+	var inputProblem *problem.Input
+	p := Decode(r, formOutput, bodyOutput)
+	if prob, ok := p.(*problem.Input); ok {
+		inputProblem = prob
+	} else {
+		return prob
+	}
+	if formOutput != nil {
 		formErrors := formOutput.Validate()
 		if len(formErrors) > 0 {
 			if inputProblem == nil {
@@ -35,12 +59,6 @@ func Decode(r *http.Request, formOutput Output, bodyOutput Output) problem.Probl
 		}
 	}
 	if bodyOutput != nil {
-		bodyDecoder := json.NewDecoder(r.Body)
-		defer r.Body.Close()
-		err := bodyDecoder.Decode(&bodyOutput)
-		if err != nil {
-			return &problem.DefaultUnexpected
-		}
 		bodyErrors := bodyOutput.Validate()
 		if len(bodyErrors) > 0 {
 			if inputProblem == nil {
